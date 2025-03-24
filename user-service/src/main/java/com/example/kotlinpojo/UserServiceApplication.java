0846df5c0e7f0fe2f127934fb66d4a367fc2db2c
Emin Yıldız <eminyildiz122@gmail.com>
2025-03-24 14:51:00 +0300

    feat: added eureka service

--- user-service/src/main/java/com/example/kotlinpojo/UserServiceApplication.java
+++ user-service/src/main/java/com/example/kotlinpojo/UserServiceApplication.java
@@ -7,12 +7,14 @@ import com.example.kotlinpojo.user.repository.UserRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.UUID;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
