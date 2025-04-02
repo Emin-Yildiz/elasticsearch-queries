bf59e1d701777101e300a8190e064b856c956247
Emin Yıldız <eminyildiz122@gmail.com>
2025-04-02 12:03:03 +0300

    feat: added search service

---
+++ search-service/src/main/java/org/example/searchservice/config/ElasticSearchConfig.java
@@ -0,0 +1,23 @@
+package org.example.searchservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+@ComponentScan(basePackages = {"org.example.searchservice"})
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${elasticsearch.url}")
+    private String elasticsearchUrl;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(elasticsearchUrl)
+                .build();
+    }
+}
+
---
+++ search-service/src/test/java/org/example/searchservice/SearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.searchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
