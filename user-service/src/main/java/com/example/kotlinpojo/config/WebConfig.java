294cf7729845b3ee3dfb48ec0960a9b8a215f4e1
Emin Yıldız <eminyildiz122@gmail.com>
2025-04-14 10:35:30 +0300

    feat: added interceptor logging

---
+++ user-service/src/main/java/com/example/kotlinpojo/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.example.kotlinpojo.config;
+
+import com.example.kotlinpojo.interceptor.LoggingInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor());
+    }
+}
---
+++ user-service/src/main/java/com/example/kotlinpojo/interceptor/LoggingInterceptor.java
@@ -0,0 +1,55 @@
+package com.example.kotlinpojo.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object)  {
+        String ipAddress = getClientIp(request);
+        logger.info("[Pre Handle] Request IP: {} | URI: {} | Method: {}",
+                ipAddress,
+                request.getRequestURI(),
+                request.getMethod());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object object, ModelAndView model){
+        String ipAddress = getClientIp(request);
+        logger.info("[Post Handle] Request IP: {} | URI: {} | Method: {} | Status: {}",
+                ipAddress,
+                request.getRequestURI(),
+                request.getMethod(),
+                response.getStatus());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object object, Exception exception){
+        String ipAddress = getClientIp(request);
+        String message = response.getStatus() == 200 ? "Request is complete" : "Request is failed";
+        logger.info("[After Completion] Request IP: {} | URI: {} | Method: {} | Status: {} | Message: {}",
+                ipAddress,
+                request.getRequestURI(),
+                request.getMethod(),
+                response.getStatus(),
+                message);
+    }
+
+    public String getClientIp(HttpServletRequest request) {
+        String header = request.getHeader("X-Forwarded-For");
+        if (header != null && !header.isEmpty()) {
+            return header.split(",")[0];
+        }
+        return request.getRemoteAddr();
+    }
+}
